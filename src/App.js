9fa6e12441c598d5ea230a79bea00324eb9a73e3
@Ash9311 <ashutoshmulki7@gmail.com>
2023-07-11 00:47:08 +0530

    products and image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+const user = {
+  name: "Ashutosh",
+  imageUrl: 'https://media.licdn.com/dms/image/D5603AQG1rUwy22YWhQ/profile-displayphoto-shrink_800_800/0/1685295925814?e=1694649600&v=beta&t=i5MJngGZwGf-X2LNx6T_5iti_s2TgmyjMarqbEH-Gb8',
+  imageSize:90
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+     
+const listItems = products.map(product=>
+  <li
+    key={product.id}
+    style={{
+      color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}
+  >
+    {product.title}
+  </li>
+  );
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+    <h1>{user.name}</h1>
+    <img className='avatar' src={user.imageUrl} alt={'photo of '+user.name} style={{width:user.imageSize,height:user.imageSize}}></img>
+    <ul>{listItems}</ul>
     </div>
   );
 }
